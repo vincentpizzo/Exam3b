2a218f7161245792bcd5d3097f1fb5b8351008f1
Vinnie Pizzo <vincent-pizzo@uiowa.edu>
2014-12-14 16:51:14 -0600

    Adding changes

--- Default.aspx
+++ Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" Culture="auto:en-US" UICulture="auto"%>
 
 <!DOCTYPE html>
 
@@ -17,21 +17,20 @@
         <div class="radio">  
         <p>
                 
-                <asp:RadioButtonList ID="RadioButtonList1" runat="server" style="list-style=center" align="center">
-                <asp:ListItem>Arabic</asp:ListItem>
-                <asp:ListItem>Chinese</asp:ListItem>
-                <asp:ListItem>English</asp:ListItem>
-                <asp:ListItem>Spanish</asp:ListItem>
-            </asp:RadioButtonList>
+                <asp:DropDownList ID="DropDownList1" runat="server">
+                    <asp:ListItem>Arabic</asp:ListItem>
+                    <asp:ListItem>Chinese</asp:ListItem>
+                    <asp:ListItem>English</asp:ListItem>
+                    <asp:ListItem>Spanish</asp:ListItem>
+                </asp:DropDownList>
                 
         </p></div>
         <div class="stuff"><p>My name is:
             <asp:TextBox ID="tb_Name" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RFV1" runat="server" ErrorMessage="Please enter your name" CssClass="validationError" ControlToValidate="tb_Name"></asp:RequiredFieldValidator>
         </p>
-        <p>I am:<asp:RadioButtonList ID="RadioButtonList2" runat="server" RepeatDirection="Horizontal" align="center">
-            <asp:ListItem>Male</asp:ListItem>
-            <asp:ListItem>Female</asp:ListItem>
-            </asp:RadioButtonList>
+        <p>I am:<asp:RadioButton ID="rdo_Male" runat="server" Text="Male" />
+            <asp:RadioButton ID="rdo_Female" runat="server" Text="Female" />
         </p>
         <p>I plan to graduate on:</p>
         <p>
@@ -39,12 +38,34 @@
         </p>
         <p>When I graduate, I hope to earn:
             <asp:TextBox ID="tb_Salary" runat="server">Enter Salary</asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Please enter your desired salary" CssClass="validationError" ControlToValidate="tb_Salary"></asp:RequiredFieldValidator>
 &nbsp;&nbsp;&nbsp;&nbsp; </p>
         <p>
             <asp:Button ID="btn_Submit" runat="server" Text="Submit" align="center"/>
         </p>
 
     </div>
+    
+    <asp:Label ID="lbl_Hello" runat="server" meta:resourceKey="lbl_Hello"></asp:Label>
+            &nbsp;<asp:Label ID="lbl_Name" runat="server"></asp:Label>
+            <asp:Label ID="lbl_User" runat="server"></asp:Label>
+
+            <asp:Label ID="lbl_Date" runat="server" meta:resourceKey="lbl_Date" Text="lbl_Date"></asp:Label>
+            <asp:Label ID="lbl_Calendar" runat="server"></asp:Label>
+
+            <asp:Label ID="lbl_Salary" runat="server" meta:resourceKey="lbl_Salary"></asp:Label>
+            <asp:Label ID="lbl_Money" runat="server"></asp:Label>
+            <asp:Label ID="lbl_Out" runat="server" meta:resourceKey="lbl_Out"></asp:Label>
+
+            <asp:Label ID="lbl_Link" runat="server" meta:resourceKey="lbl_Link"></asp:Label>
+            <asp:LinkButton ID="btn_Link" runat="server">Github</asp:LinkButton>
+
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="btn_Reset" runat="server" meta:resourceKey="btn_reset" Text="Reset Page" align="center"/>
+
+        <asp:Label ID="lbl_Language" runat="server" meta:resourceKey="lbl_Language" ></asp:Label>
     </form>
 </body>
 </html>
--- Default.aspx.vb
+++ Default.aspx.vb
@@ -1,8 +1,124 @@
-﻿
+﻿Imports System.Threading
+Imports System.Globalization
+
+
 Partial Class _Default
     Inherits System.Web.UI.Page
 
-    Protected Sub btn_Submit_Click(sender As Object, e As EventArgs) Handles btn_Submit.Click
+    Protected Sub Language()
+        Dim language As String = Request("dd_languages")
+
+        If language IsNot Nothing Or language <> "" Then
+            Thread.CurrentThread.CurrentUICulture = New CultureInfo(language)
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language)
+        End If
+    End Sub
+
+
+
+
+    Protected Sub button_submit_Click(sender As Object, e As EventArgs) Handles btn_Submit.Click
+
+
+        If rdo_Male.Checked Then
+            lbl_Name.Text = "Mr. "
+        End If
+
+        If rdo_Female.Checked Then
+            lbl_Name.Text = "Ms. "
+        End If
+        lbl_Calendar.Text = System.Web.UI.WebControls.Calendar.SelectedDate.ToShortDateString()
+
+        Dim salary As Decimal = Val(tb_Salary.Text)
+        lbl_Salary.Text = String.Format("{0:C}", salary)
+
+        lbl_User.Text = StrConv(tb_Name.Text, VbStrConv.ProperCase)
+
+        ' show the output
+        lbl_Hello.Visible = True
+        lbl_Name.Visible = True
+        lbl_Name.Visible = True
+        lbl_Date.Visible = True
+        lbl_Calendar.Visible = True
+        lbl_Salary.Visible = True
+        lbl_Money.Visible = True
+        lbl_Out.Visible = True
+        lbl_Link.Visible = True
+        btn_Link.Visible = True
+        btn_Reset.Visible = True
+
+        ' don't show the first page of stuff
+        tb_Name.Visible = False
+        rdo_Male.Visible = False
+        rdo_Female.Visible = False
+        System.Web.UI.WebControls.Calendar.Visible = False
+        tb_Salary.Visible = False
+        btn_Submit.Visible = False
+
+    End Sub
+
+    Protected Sub GitHub_Click(sender As Object, e As EventArgs) Handles btn_Link.Click
+
+        Response.Redirect("https://github.com/vincentpizzo")
+
+    End Sub
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+
+        System.Web.UI.WebControls.Calendar.VisibleDate = Today
+
+        tb_Name.Focus()
+
+        lbl_Hello.Visible = False
+        lbl_Name.Visible = False
+        lbl_User.Visible = False
+        lbl_Date.Visible = False
+        lbl_Calendar.Visible = False
+        lbl_Salary.Visible = False
+        lbl_Money.Visible = False
+        lbl_Out.Visible = False
+        lbl_Link.Visible = False
+        btn_Link.Visible = False
+        btn_Reset.Visible = False
+
+
+
+    End Sub
+
+    Protected Sub button_restart_Click(sender As Object, e As EventArgs) Handles button_restart.Click
+
+        tb_Name.Visible = True
+        rdo_Male.Visible = True
+        rdo_Female.Visible = True
+        System.Web.UI.WebControls.Calendar.Visible = True
+        tb_Salary.Visible = True
+        btn_Submit.Visible = True
+
+
+
+
+        lbl_Hello.Visible = False
+        lbl_Name.Visible = False
+        lbl_User.Visible = False
+        lbl_Date.Visible = False
+        lbl_Calendar.Visible = False
+        lbl_Salary.Visible = False
+        lbl_Money.Visible = False
+        lbl_Out.Visible = False
+        lbl_Link.Visible = False
+        btn_Link.Visible = False
+        btn_Reset.Visible = False
+
+
+        ' clear all input fields
+        tb_Name.Text = String.Empty
+        tb_Salary.Text = String.Empty
+        rdo_Male.Checked = False
+        rdo_Female.Checked = False
+        System.Web.UI.WebControls.Calendar.SelectedDate = Today
+        System.Web.UI.WebControls.Calendar.VisibleDate = Today
+
 
     End Sub
 End Class
